6bbdb74660225b4a4d37a7814557e790ac66b83a
Russ Kern <30536888+CreativePoweredGeek@users.noreply.github.com>
2020-05-22 13:48:42 -0400

    Create addon.setup.php

---
+++ addon.setup.php
@@ -0,0 +1,9 @@
+<?php
+return array(
+  'author'      => 'Russ Kern',
+  'author_url'  => 'https://kerncreativedesign.com',
+  'name'        => 'KC_datatools',
+  'description' => 'Random collection of Tools written to manage data',
+  'version'     => '1.1',
+  'namespace'   => 'KernCreativeDesign\KC_datatools'
+);
